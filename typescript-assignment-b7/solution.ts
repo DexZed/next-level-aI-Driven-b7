1949e5c0080945a4237268ae28cfd309876f4097
DexZed <144045080+DexZed@users.noreply.github.com>
2026-05-07 18:28:05 +0600

    `Added new functions and classes for filtering, string manipulation, type checking, property access, and more`

--- typescript-assignment-b7/solution.ts
+++ typescript-assignment-b7/solution.ts
@@ -1,1 +1,60 @@
-console.log("Hello via Bun!");
+function filterEvenNumbers(numbers: number[]): number[] {
+  return numbers.filter((number) => number % 2 === 0);
+}
+
+function reverseString(statment: string): string {
+  return statment.split("").reverse().join("");
+}
+
+function checkType(args: number | string): number | string {
+  switch (typeof args) {
+    case "string":
+      return "String";
+    case "number":
+      return "Number";
+  }
+}
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isRead?: boolean;
+}
+
+function toggleReadStatus(book: Book): Book {
+  return { ...book, isRead: !book.isRead };
+}
+const myBook = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+};
+
+abstract class Person {
+  public name: string;
+  public age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  abstract getDetails(): string;
+}
+class Student extends Person {
+  private grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter((num) => arr2.includes(num));
+}
