d2fad5dd0a23c00f299e1b61234a6a9351bbdec7
DexZed <144045080+DexZed@users.noreply.github.com>
2026-05-22 17:49:46 +0600

    interface definition

---
+++ dpulse-server/src/utils/interfaces.ts
@@ -0,0 +1,21 @@
+export interface User{
+    id:string,
+    name:string,
+    email:string,
+    password:string,
+    role:'contributor'|'maintainer'
+    created_at:Date,
+    updated_at:Date
+}
+
+export interface Issue {
+    id:string,
+    title:string,
+    description:string,
+    type:'bug'|'feature_request',
+    status:'open'|'in_progress'|'resolved',
+    reporter_id:string,
+    created_at:Date,
+    updated_at:Date
+
+}
