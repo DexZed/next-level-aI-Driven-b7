f19d4981eb47c686668bb0c0946cce393462caaf
DexZed <144045080+DexZed@users.noreply.github.com>
2026-06-14 18:41:20 +0600

    SQL TYPE SETTING

--- sql-assign/QUERY.sql
+++ sql-assign/QUERY.sql
@@ -4,86 +4,177 @@
 -- INSTRUCTIONS: Replace 'TYPE' and the constraint placeholders with your own
 --               actual data types, relational keys, and check criteria.
 -- =========================================================================
-
 -- DROP TABLES IF THEY ALREADY EXIST TO PREVENT CONFLICTS
 DROP TABLE IF EXISTS Bookings;
+
 DROP TABLE IF EXISTS Matches;
+
 DROP TABLE IF EXISTS Users;
 
 -- =========================================================================
 -- 1. CREATE USERS TABLE
 -- =========================================================================
-CREATE TABLE Users (
-    user_id TYPE,
-    full_name TYPE,
-    email TYPE,
-    role TYPE,
-    phone_number TYPE,
-    
-    -- Write your constraint to make 'user_id' the Primary Key
-    -- Write your constraint to ensure 'email' values are never duplicated
-    -- Write your check constraint to restrict 'role' to specific allowed strings
-);
+CREATE TABLE
+    Users (
+        user_id INT GENERATED ALWAYS AS IDENTITY,
+        full_name VARCHAR(16) NOT NULL,
+        email VARCHAR(32) NOT NULL,
+        role ENUM ('Football Fan', 'Ticket Manager') NOT NULL,
+        phone_number INT NULL,
+        -- Write your constraint to make 'user_id' the Primary Key
+        PRIMARY KEY (user_id),
+        -- Write your constraint to ensure 'email' values are never duplicated
+        UNIQUE (email),
+        -- Write your check constraint to restrict 'role' to specific allowed strings
+        CHECK (role IN ('Football Fan', 'Ticket Manager'))
+    );
 
 -- =========================================================================
 -- 2. CREATE MATCHES TABLE
 -- =========================================================================
-CREATE TABLE Matches (
-    match_id TYPE,
-    fixture TYPE,
-    tournament_category TYPE,
-    base_ticket_price TYPE,
-    match_status TYPE,
-    
-    -- Write your constraint to make 'match_id' the Primary Key
-    -- Write your check constraint to prevent negative ticket prices
-    -- Write your check constraint to restrict 'match_status' values
-);
+CREATE TABLE
+    Matches (
+        match_id INT GENERATED ALWAYS AS IDENTITY,
+        fixture VARCHAR(32) NOT NULL,
+        tournament_category ENUM ('Champions League', 'Premier League', 'Serie A') NOT NULL, 
+        base_ticket_price DECIMAL(10, 2) NOT NULL,
+        match_status ENUM ('Available', 'Sold Out', 'Selling Fast','Postponed') NOT NULL,
+        -- Write your constraint to make 'match_id' the Primary Key
+        PRIMARY KEY (match_id),
+        -- Write your check constraint to prevent negative ticket prices
+        CHECK (base_ticket_price >= 0),
+        -- Write your check constraint to restrict 'match_status' values
+        CHECK (match_status IN ('Available', 'Sold Out', 'Selling Fast','Postponed')),
+    );
 
 -- =========================================================================
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
-CREATE TABLE Bookings (
-    booking_id TYPE,
-    user_id TYPE,
-    match_id TYPE,
-    seat_number TYPE,
-    payment_status TYPE,
-    total_cost TYPE,
-    
-    -- Write your constraint to make 'booking_id' the Primary Key
-    -- Write your Foreign Key constraint linking 'user_id' to the Users table
-    -- Write your Foreign Key constraint linking 'match_id' to the Matches table
-    -- Write your check constraint to ensure 'total_cost' is non-negative
-    -- Write your check constraint to restrict 'payment_status' values
-);
+CREATE TABLE
+    Bookings (
+        booking_id INT GENERATED ALWAYS AS IDENTITY,
+        user_id INT NOT NULL,
+        match_id INT NOT NULL,
+        seat_number VARCHAR(16) NULL,
+        payment_status ENUM ('Pending', 'Confirmed','Cancelled','Refunded') NULL
+        total_cost DECIMAL(10, 2) NULL,
+        -- Write your constraint to make 'booking_id' the Primary Key
+        PRIMARY KEY (booking_id),
+        -- Write your Foreign Key constraint linking 'user_id' to the Users table
+        INDEX fk_user_id (user_id),
+        INDEX fk_match_id (match_id),
 
+       CONSTRAINT fk_user_id 
+        FOREIGN KEY (user_id) 
+        REFERENCES Users(user_id),
+        -- Write your Foreign Key constraint linking 'match_id' to the Matches table
+        CONSTRAINT fk_match_id 
+        FOREIGN KEY (match_id) 
+        REFERENCES Matches(match_id),
+        -- Write your check constraint to ensure 'total_cost' is non-negative
+        CHECK (total_cost >= 0),
+        -- Write your check constraint to restrict 'payment_status' values
+        CHECK (payment_status IN ('Pending', 'Confirmed','Cancelled','Refunded'))
+    );
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
-INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
-(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
-(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
-(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
-(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+INSERT INTO
+    Users (user_id, full_name, email, role, phone_number)
+VALUES
+    (
+        1,
+        'Tanvir Rahman',
+        'tanvir@mail.com',
+        'Football Fan',
+        '+8801711111111'
+    ),
+    (
+        2,
+        'Asif Haque',
+        'asif@mail.com',
+        'Football Fan',
+        '+8801722222222'
+    ),
+    (
+        3,
+        'Sajjad Rahman',
+        'sajjad@mail.com',
+        'Ticket Manager',
+        '+8801733333333'
+    ),
+    (
+        4,
+        'Jannat Ara',
+        'jannat@mail.com',
+        'Football Fan',
+        NULL
+    );
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
 -- =========================================================================
-INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
-(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
-(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
-(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
-(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
-(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+INSERT INTO
+    Matches (
+        match_id,
+        fixture,
+        tournament_category,
+        base_ticket_price,
+        match_status
+    )
+VALUES
+    (
+        101,
+        'Real Madrid vs Barcelona',
+        'Champions League',
+        150.00,
+        'Available'
+    ),
+    (
+        102,
+        'Man City vs Liverpool',
+        'Premier League',
+        120.00,
+        'Selling Fast'
+    ),
+    (
+        103,
+        'Bayern Munich vs PSG',
+        'Champions League',
+        130.00,
+        'Available'
+    ),
+    (
+        104,
+        'AC Milan vs Inter Milan',
+        'Serie A',
+        90.00,
+        'Sold Out'
+    ),
+    (
+        105,
+        'Juventus vs Roma',
+        'Serie A',
+        80.00,
+        'Available'
+    );
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
 -- =========================================================================
-INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
-(501, 1, 101, 'A-12', 'Confirmed', 150.00),
-(502, 1, 102, 'B-04', 'Confirmed', 120.00),
-(503, 2, 101, 'A-13', 'Confirmed', 150.00),
-(504, 2, 101, NULL, NULL, 150.00),
-(505, 3, 102, 'C-20', 'Pending', 120.00);
+INSERT INTO
+    Bookings (
+        booking_id,
+        user_id,
+        match_id,
+        seat_number,
+        payment_status,
+        total_cost
+    )
+VALUES
+    (501, 1, 101, 'A-12', 'Confirmed', 150.00),
+    (502, 1, 102, 'B-04', 'Confirmed', 120.00),
+    (503, 2, 101, 'A-13', 'Confirmed', 150.00),
+    (504, 2, 101, NULL, NULL, 150.00),
+    (505, 3, 102, 'C-20', 'Pending', 120.00);
