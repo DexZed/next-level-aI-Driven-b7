60f4bdd5d9c4363d9bf8774f922981e111bcdc69
DexZed <144045080+DexZed@users.noreply.github.com>
2026-07-30 19:52:29 +0600

    completed admin

--- fixitnow/src/modules/admin/adminServices.ts
+++ fixitnow/src/modules/admin/adminServices.ts
@@ -3,34 +3,70 @@ import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { db } from "../../prisma/db.js";
 
-export const allUsers = asyncWrapper(async (req: Request, res: Response) => {
-    const users = await db.orm.public.User.select("id", "email", "name", "role",)
+export const allUsers = asyncWrapper(async (_: Request, res: Response) => {
+    const users = await db.orm.public.User.select("id", "email", "name", "role", "status").all()
     res.status(StatusCodes.OK).json({
-        message: `from path ${req.path} and method ${req.method}`,
-        users
+        message: "Users Fetched Successfully",
+        data: users
     })
 })
 
-export const allBookings = asyncWrapper(async (req: Request, res: Response) => {
+export const allBookings = asyncWrapper(async (_: Request, res: Response) => {
+
+    const bookings = db.orm.public.Booking.select('id', 'user_id', 'technician_id', 'service_id', 'status', 'scheduled_at', 'total_price', 'created_at', 'updated_at').all()
     res.status(StatusCodes.OK).json({
-        message: `from path ${req.path} and method ${req.method}`
+        message: "Bookings Fetched Successfully",
+        data: bookings
     })
 })
 
-export const allCategories = asyncWrapper(async (req: Request, res: Response) => {
+export const allCategories = asyncWrapper(async (_: Request, res: Response) => {
+    const categories = await db.orm.public.Category.select('id', 'name', 'description', 'is_active').all()
     res.status(StatusCodes.OK).json({
-        message: `from path ${req.path} and method ${req.method}`
+        message: "Categories Fetched Successfully",
+        data: categories
     })
 })
 
 export const createServiceCategory = asyncWrapper(async (req: Request, res: Response) => {
+    const { name, description, is_active } = req.body
+    if (!name || !description || !is_active) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            message: "All fields are required",
+        })
+        return;
+    }
+    const existingCategory = await db.orm.public.Category.where({ name }).first();
+    if (existingCategory) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            message: "Category already exists",
+        })
+        return;
+    }
+    const category = await db.orm.public.Category.create({
+        name,
+        description,
+        is_active
+
+    })
     res.status(StatusCodes.OK).json({
-        message: `from path ${req.path} and method ${req.method}`
+        message: "Category Created Successfully",
+        data: category
     })
 })
 
 export const updateUserStatus = asyncWrapper(async (req: Request, res: Response) => {
+    const { id, status } = req.body
+    const user = await db.orm.public.User.where({ id }).first();
+    if (!user) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+            message: "User not found",
+        })
+        return;
+    }
+    const updatedUser = await db.orm.public.User.where({ id }).update({ status })
     res.status(StatusCodes.OK).json({
-        message: `from path ${req.path} and method ${req.method}`
+        message: "User Status Updated Successfully",
+        data: updatedUser
     })
 })
